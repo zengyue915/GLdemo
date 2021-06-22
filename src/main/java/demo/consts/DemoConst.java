c4302f29b25c222ecf5e5598580bf1025405fd15
Calvin Nguyen <calvinnguyen3563@gmail.com>
2021-06-22 04:26:46 +0100

    add new query for comment table

--- src/main/java/demo/consts/DemoConst.java
+++ src/main/java/demo/consts/DemoConst.java
@@ -2,9 +2,9 @@ package demo.consts;
 
 public class DemoConst {
 
-    public static final String UPLOAD_FILE_LOCATION = "/Users/itsyuezeng/Desktop/JavaProjects/DLdemo/upload";
+    public static final String UPLOAD_FILE_LOCATION = "C:\\Users\\calvi\\Desktop\\yue_project\\GLdemo\\upload";
 
-    public static final String DOWNLOAD_FILE_LOCATION = "/Users/itsyuezeng/Desktop/JavaProjects/DLdemo/download";
+    public static final String DOWNLOAD_FILE_LOCATION = "C:\\Users\\calvi\\Desktop\\yue_project\\GLdemo\\download";
 
     //public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/geo_db?characterEncoding=utf-8&&useSSL=false&&serverTimezone=PST";
     public static String DB_URL = "jdbc:mysql://127.0.0.1:3306/geo_db?" +
--- src/main/java/demo/dao/CoordinatesMapper.java
+++ src/main/java/demo/dao/CoordinatesMapper.java
@@ -21,50 +21,97 @@ public interface CoordinatesMapper {
     //@Select ("SELECT T1.id FROM ( SELECT id, st_astext(cords), #{text} as planar_degrees FROM Coordinates ORDER BY planar_degrees LIMIT #{Count}) AS T1")
     List<String> listCoordinates(@Param("text")String text, @Param("Count") Integer count);
 
-    @Select ("SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment " +
-            "FROM ( SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment, st_astext(cords), #{text} as planar_degrees " +
-            "FROM Coordinates ORDER BY planar_degrees LIMIT #{Count}) AS T1")
+    @Select ("WITH T1 AS (\n" +
+            "\tSELECT coord.ID, coord.RA, coord.DE, coord.MagFilter, coord.MagBrightness, coord.MagFaintest, \n" +
+            "\t\t   coord.QSOorigin, coord.Method, coord.PossibleType, coord.CandidateStatus, coord.Notes,\n" +
+            "\t\t   GROUP_CONCAT(DISTINCT CONCAT(comm.datetime,' ', comm.comment) ORDER BY comm.datetime SEPARATOR '\\n') AS comment, st_astext(coord.cords),\n" +
+            "           #{text} AS planar_degrees\n" +
+            "\tFROM Coordinates AS coord\n" +
+            "    LEFT JOIN comments AS comm \n" +
+            "\t\tON coord.ID = comm.coordinate_id\n" +
+            "\tGROUP BY coord.ID\n" +
+            "    ORDER BY planar_degrees\n" +
+            "    LIMIT #{count}\n" +
+            ")\n" +
+            "SELECT ID, RA, DE, MagFilter, MagBrightness, MagFaintest, QSOorigin, Method,\n" +
+            "       PossibleType, CandidateStatus, Notes\n" +
+            "FROM T1")
     List<Coordinate> listCoordinatesInfo(@Param("text")String text, @Param("Count") Integer count);
 
 
-    @Select ("SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment " +
-            "FROM ( SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment, st_astext(cords), st_distance(st_geomfromtext(#{point} , 4326), Coordinates.cords) as planar_degrees " +
-            "FROM Coordinates ORDER BY planar_degrees LIMIT #{Count}) AS T1")
+    @Select ("WITH T1 AS (\n" +
+            "\tSELECT coord.ID, coord.RA, coord.DE, coord.MagFilter, coord.MagBrightness, coord.MagFaintest, \n" +
+            "\t\t   coord.QSOorigin, coord.Method, coord.PossibleType, coord.CandidateStatus, coord.Notes,\n" +
+            "\t\t   GROUP_CONCAT(DISTINCT CONCAT(comm.datetime,' ', comm.comment) ORDER BY comm.datetime SEPARATOR '\\n') AS comment, st_astext(coord.cords),\n" +
+            "           st_distance(st_geomfromtext(#{point} , 4326), coord.cords) AS planar_degrees\n" +
+            "\tFROM Coordinates AS coord\n" +
+            "    LEFT JOIN comments AS comm \n" +
+            "\t\tON coord.ID = comm.coordinate_id\n" +
+            "\tGROUP BY coord.ID\n" +
+            "    ORDER BY planar_degrees\n" +
+            ")\n" +
+            "SELECT ID, RA, DE, MagFilter, MagBrightness, MagFaintest, QSOorigin, Method,\n" +
+            "       PossibleType, CandidateStatus, Notes, Comment\n" +
+            "FROM T1\n" +
+            "LIMIT #{Count}")
     List<Coordinate> listCoordinatesInfoLimit(@Param("point") String point, @Param("Count") Integer count);
 
-    @Select ("SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment " +
-            "FROM ( SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment, st_astext(cords), st_distance(st_geomfromtext(#{point} , 4326), Coordinates.cords) as planar_degrees " +
-            "FROM Coordinates ORDER BY planar_degrees) AS T1")
+    @Select ("WITH T1 AS (\n" +
+            "\tSELECT coord.ID, coord.RA, coord.DE, coord.MagFilter, coord.MagBrightness, coord.MagFaintest, \n" +
+            "\t\t   coord.QSOorigin, coord.Method, coord.PossibleType, coord.CandidateStatus, coord.Notes,\n" +
+            "\t\t   GROUP_CONCAT(DISTINCT CONCAT(comm.datetime,' ', comm.comment) ORDER BY comm.datetime SEPARATOR '\\n') AS comment, st_astext(coord.cords),\n" +
+            "           st_distance(st_geomfromtext(#{point} , 4326), coord.cords) AS planar_degrees\n" +
+            "\tFROM Coordinates AS coord\n" +
+            "    LEFT JOIN comments AS comm \n" +
+            "\t\tON coord.ID = comm.coordinate_id\n" +
+            "\tGROUP BY coord.ID\n" +
+            "    ORDER BY planar_degrees\n" +
+            ")\n" +
+            "SELECT ID, RA, DE, MagFilter, MagBrightness, MagFaintest, QSOorigin, Method,\n" +
+            "       PossibleType, CandidateStatus, Notes, Comment\n" +
+            "FROM T1")
     List<Coordinate> listCoordinatesInfoAll(@Param("point") String point);
 
-    @Select ("SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment " +
-            "FROM ( SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment, st_astext(cords), st_distance(st_geomfromtext(#{point} , 4326), Coordinates.cords) as planar_degrees " +
-            "FROM Coordinates ORDER BY planar_degrees LIMIT #{Count}) AS T1 "+
-            "ORDER BY ${order_by}")
+    @Select ("WITH T1 AS (\n" +
+            "\tSELECT coord.ID, coord.RA, coord.DE, coord.MagFilter, coord.MagBrightness, coord.MagFaintest, \n" +
+            "\t\t   coord.QSOorigin, coord.Method, coord.PossibleType, coord.CandidateStatus, coord.Notes,\n" +
+            "\t\t   GROUP_CONCAT(DISTINCT CONCAT(comm.datetime,' ', comm.comment) ORDER BY comm.datetime SEPARATOR '\\n') AS comment, st_astext(coord.cords),\n" +
+            "           st_distance(st_geomfromtext(#{point} , 4326), coord.cords) AS planar_degrees\n" +
+            "\tFROM Coordinates AS coord\n" +
+            "    LEFT JOIN comments AS comm \n" +
+            "\t\tON coord.ID = comm.coordinate_id\n" +
+            "\tGROUP BY coord.ID\n" +
+            "    ORDER BY ${order_by}\n" +
+            ")\n" +
+            "SELECT ID, RA, DE, MagFilter, MagBrightness, MagFaintest, QSOorigin, Method,\n" +
+            "       PossibleType, CandidateStatus, Notes, Comment\n" +
+            "FROM T1\n" +
+            "LIMIT #{Count}")
     List<Coordinate> listCoordinatesInfoLimitByOrder(@Param("point") String point, @Param("Count") Integer count, @Param("order_by") String order);
 
 
 
-    @Select ("SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment " +
-            "FROM ( SELECT ID,RA,DE,MagFilter,MagBrightness,MagFaintest,QSOorigin,Method," +
-            "PossibleType,CandidateStatus,Notes,Comment, st_astext(cords), st_distance(st_geomfromtext(#{point} , 4326), Coordinates.cords) as planar_degrees " +
-            "FROM Coordinates ORDER BY planar_degrees) AS T1 "+
-            "ORDER BY ${order_by}")
+    @Select ("WITH T1 AS (\n" +
+            "\tSELECT coord.ID, coord.RA, coord.DE, coord.MagFilter, coord.MagBrightness, coord.MagFaintest, \n" +
+            "\t\t   coord.QSOorigin, coord.Method, coord.PossibleType, coord.CandidateStatus, coord.Notes,\n" +
+            "\t\t   GROUP_CONCAT(DISTINCT CONCAT(comm.datetime,' ', comm.comment) ORDER BY comm.datetime SEPARATOR '\\n') AS comment, st_astext(coord.cords),\n" +
+            "           st_distance(st_geomfromtext(#{point} , 4326), coord.cords) AS planar_degrees\n" +
+            "\tFROM Coordinates AS coord\n" +
+            "    LEFT JOIN comments AS comm \n" +
+            "\t\tON coord.ID = comm.coordinate_id\n" +
+            "\tGROUP BY coord.ID\n" +
+            "    ORDER BY #{order_by}\n" +
+            ")\n" +
+            "SELECT ID, RA, DE, MagFilter, MagBrightness, MagFaintest, QSOorigin, Method,\n" +
+            "       PossibleType, CandidateStatus, Notes, Comment\n" +
+            "FROM T1")
     List<Coordinate> listCoordinatesInfoAllByOrder(@Param("point") String point, @Param("order_by") String order);
 
 
     @Select ("SELECT Notes FROM Coordinates WHERE id = #{id} LIMIT 2")
     List<String> getNotesById(@Param("id") String id);
 
-    @Update("UPDATE Coordinates SET COMMENT = concat(COMMENT, #{comments}) WHERE ID = #{id}")
+    @Insert("INSERT INTO comments (coordinate_id, comment) VALUES (#{id}, #{comments})")
+    // @Update("UPDATE Coordinates SET COMMENT = concat(COMMENT, #{comments}) WHERE ID = #{id}")
     void updateComment(@Param("comments") String comments, @Param("id") String id);
 }
